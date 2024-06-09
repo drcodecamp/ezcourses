9fe6b32f22812e9f05ccbbbdeac3989f2cf3202a
doctorcode <info@doctorcode.org>
2024-06-09 03:15:34 +0300

    style: fix focus state for inputs

--- shared/input/email.tsx
+++ shared/input/email.tsx
@@ -2,47 +2,46 @@ import classes from './inputs.module.css'
 import React from 'react'
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
-    height?: string
-    variant?: string
-    endContent?: React.ReactNode
-    placeholder?: string
-    type?: string
-    value?: string
-    required: boolean
-    disabled?: boolean
-    pattern?: RegExp
-    fullWidth?: boolean
-    hideStrength?: boolean
-    width?: string
-    valid: boolean | null
+  height?: string
+  variant?: string
+  endContent?: React.ReactNode
+  placeholder?: string
+  type?: string
+  value?: string
+  required: boolean
+  disabled?: boolean
+  pattern?: RegExp
+  fullWidth?: boolean
+  hideStrength?: boolean
+  width?: string
+  valid: boolean | null
 }
 
 export const EmailInput: React.FC<Props> = ({
-                                                height,
-                                                variant,
-                                                endContent,
-                                                placeholder,
-                                                value,
-                                                pattern,
-                                                fullWidth,
-                                                valid,
-                                                hideStrength,
-                                                ...props
-                                            }) => {
-    return (
-        <div className={classes.container}>
-            {endContent && <button>{endContent}</button>}
-            <input
-                value={value}
-                className={`${classes.input} ${
-                    fullWidth ? classes.full : props.width
-                } `}
-                {...props}
-            />
-            <label
-                className={valid === null ? '' : valid ? '' : classes.invalid}>
-                {placeholder}
-            </label>
-        </div>
-    )
+  height,
+  variant,
+  endContent,
+  placeholder,
+  value,
+  pattern,
+  fullWidth,
+  valid,
+  hideStrength,
+  ...props
+}) => {
+  return (
+    <div className={classes.container}>
+      {endContent && <button>{endContent}</button>}
+      <input
+        value={value}
+        className={`${classes.input} ${
+          fullWidth ? classes.full : props.width
+        } `}
+        {...props}
+      />
+      <label className={valid === null ? '' : valid ? '' : classes.invalid}>
+        {placeholder}
+      </label>
+    </div>
+  )
 }
