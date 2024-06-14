1fcba7c2e930e22b86f70e13a3a1c87c9555d355
Yanai Edri <yanai101@gmail.com>
2024-06-14 15:43:07 +0300

    refactor: improve side-nav styling and behavior

--- shared/side-nav/side-nav.tsx
+++ shared/side-nav/side-nav.tsx
@@ -31,63 +31,56 @@ export default function SideNav() {
                     </Link>
                 </nav>
                 <nav className={classes.nav}>
-                    <h3>You</h3>
-                    <Link href={appRoutes.home}>
-                        <AiOutlineFundProjectionScreen size={22}/> Your courses
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <AiOutlineHistory size={22}/> History
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <AiOutlineLike size={22}/> Liked
-                    </Link>
+                    <details>
+                        <summary> <h3>Subscriptions</h3></summary>
+                        <Link href={appRoutes.home}>
+                            <Avatar>Doctor Code</Avatar>
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <Avatar>Doctor Code</Avatar>
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <Avatar>Doctor Code</Avatar>
+                        </Link>
+                </details>
                 </nav>
                 <nav className={classes.nav}>
-                    <h3>Subscriptions</h3>
-                    <Link href={appRoutes.home}>
-                        <Avatar>Doctor Code</Avatar>
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <Avatar>Doctor Code</Avatar>
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <Avatar>Doctor Code</Avatar>
-                    </Link>
-                </nav>
-                <nav className={classes.nav}>
-                    <h3>Explore</h3>
-                    <Link href={appRoutes.home}>
-                        <AiOutlineFire size={22}/>
-                        Trending
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <LuAtom size={22}/>
-                        A.I
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <AiOutlineCode size={22}/>
-                        Development
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <AiOutlineMoneyCollect size={22}/>
-                        Business
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <BiMusic size={22}/>
-                        Music
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <AiOutlineAreaChart size={22}/>
-                        Trading
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <BiPurchaseTagAlt size={22}/>
-                        Marketing
-                    </Link>
-                    <Link href={appRoutes.home}>
-                        <FaBagShopping size={22}/>
-                        Lifestyle
-                    </Link>
+                    <details>
+                        <summary><h3>Explore</h3></summary>
+
+                        <Link href={appRoutes.home}>
+                            <AiOutlineFire size={22}/>
+                            Trending
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <LuAtom size={22}/>
+                            A.I
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <AiOutlineCode size={22}/>
+                            Development
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <AiOutlineMoneyCollect size={22}/>
+                            Business
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <BiMusic size={22}/>
+                            Music
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <AiOutlineAreaChart size={22}/>
+                            Trading
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <BiPurchaseTagAlt size={22}/>
+                            Marketing
+                        </Link>
+                        <Link href={appRoutes.home}>
+                            <FaBagShopping size={22}/>
+                            Lifestyle
+                        </Link>
+                    </details>
                 </nav>
             </aside>
         </>
