37229fde93aeed531f9fadf99ce0d6c61f6276ca
doctorcode <info@doctorcode.org>
2024-06-15 23:11:29 +0300

    docs: updates .env.example with new variables

--- api/mux-webhook/route.tsx
+++ api/mux-webhook/route.tsx
@@ -5,12 +5,12 @@ import { NextRequest } from 'next/server'
  * @param {Request} request - The request object representing the POST request.
  */
 export async function POST(request: NextRequest) {
-    const body = await request.json()
-    const { type, data } = body
-    if (type === 'video.asset.ready') {
-      // todo save this
-    } else {
-      /* handle other event types */
-    }
-    return Response.json({ message: 'ok' })
+  const body = await request.json()
+  const { type, data } = body
+  if (type === 'video.asset.ready') {
+    // todo save this
+  } else {
+    /* handle other event types */
+  }
+  return Response.json({ message: 'ok' })
 }
